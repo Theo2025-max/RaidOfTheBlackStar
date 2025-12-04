281db6c26fc6f98781a831c79cb1aa3f0e2bfe7c
Theo2025-max <madzunyetheo@gmail.com>
2025-12-04 19:53:00 +0200

    [2025/12/04]
    -Add PlayerMovement.cs for handling core player movement logic

--- RaidOfTheBlackStar/Assets/Scripts/Player/PlayerMovement.cs
+++ RaidOfTheBlackStar/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void OnMove(InputValue value)
+    {
+
     }
 }
