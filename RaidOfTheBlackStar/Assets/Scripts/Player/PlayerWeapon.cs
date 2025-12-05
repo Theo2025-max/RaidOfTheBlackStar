98f9b907f88565d28fdcdef6eb31dddeec8f524f
Theo2025-max <madzunyetheo@gmail.com>
2025-12-05 17:37:53 +0200

    [2025/12/05]
    -WIP: Crosshair setup and movement linked to player position

--- RaidOfTheBlackStar/Assets/Scripts/Player/PlayerWeapon.cs
+++ RaidOfTheBlackStar/Assets/Scripts/Player/PlayerWeapon.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.ObjectModel;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -5,17 +6,24 @@ using UnityEngine.InputSystem;
 public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField] GameObject[] lasers;
-    
+    [SerializeField] RectTransform crosshair;
+
     bool isFiring = false;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
 
     public void OnFire(InputValue value)
     {
-       isFiring = value.isPressed;
+        isFiring = value.isPressed;
     }
 
     private void Update()
     {
         ProcessFiring();
+        MoveCrosshair();
     }
 
     private void ProcessFiring()
@@ -24,9 +32,14 @@ public class PlayerWeapon : MonoBehaviour
         {
             var emissionModule = laser.GetComponent<ParticleSystem>().emission;
             emissionModule.enabled = isFiring;
-
         }
+    }
 
-
+    private void MoveCrosshair()
+    {
+        if (Mouse.current != null)
+        {
+            crosshair.position = Mouse.current.position.ReadValue();
+        }
     }
 }
